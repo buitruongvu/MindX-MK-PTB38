2fa3ee4cb2a49cd96f3b5b70afd42cb2912e3ed3
Bùi Trường Vũ <buitruongvu@MacBookAircuaBuiTruongVu.local>
2026-05-28 13:56:03 +0700

    first commit

---
+++ Lesson01/main.py
@@ -0,0 +1,28 @@
+print("Hello World!")
+my_name = "Bùi Trường Vũ" #snake case
+# name_input = input("What is your name? ") 
+# print("Hello", name_input)
+print("Bảng cửu chương 2")
+print("2 x 1 =", 2*1)
+print("2 x 2 =", 2*2)
+print("2 x 3 =", 2*3)
+my_age = 24
+# f-string
+print(f"My name is {my_name}, {my_age} year old")
+# Data types (Kiểu dữ liệu)
+# kiểu int 
+number = 86
+number1 = 68
+# kiểu float
+number3 = 6.8
+number4 = 12.0
+# kiểu Boolean
+is_student = True
+is_female = False
+# Kiểu string
+school = "BK HN"
+#Quy tắc đặt tên biến:
+#Tên biến phải bao gồm chữ hoa, chữ thường, số và dấu _ (underscore)
+# Tên biến không được bắt đầu bằng số
+# Tên biến không được trùng với từ khoá trong python
+
