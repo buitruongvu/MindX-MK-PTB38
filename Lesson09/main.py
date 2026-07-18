a408914456ed5c1f95ea84208749c8698f3649c5
Bùi Trường Vũ <buitruongvu@MacBook-Pro.local>
2026-07-18 16:08:55 +0700

    update lesson09

---
+++ Lesson09/main.py
@@ -0,0 +1,13 @@
+numbers = [1, 2, 3, 4, 5, 6]
+animal = ["Dogs", "Cats", "Bear"]
+hypers = [1, 2.3, "hello", True]
+print(hypers[2])
+print(animal[0])
+print(numbers[4])
+
+for number in numbers:
+  print(number, end=" ")
+print("")
+for pet in animal:
+  print(pet, end=" ")
+
