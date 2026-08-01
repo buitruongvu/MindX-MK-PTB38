589468bf112b72b4175dcfcc309dd35950d9a4a7
Bùi Trường Vũ <buitruongvu@MacBook-Pro.local>
2026-08-01 16:00:50 +0700

    update lesson11

--- Lesson11/practices.py
+++ Lesson11/practices.py
@@ -1,3 +1,11 @@
+# Bài 1: Khởi tạo và Duyệt xâu
+# Yêu cầu người dùng nhập vào một chuỗi văn bản bất kỳ từ bàn phím. Hãy viết
+# chương trình thực hiện các công việc sau:
+# Duyệt qua chuỗi và in ra màn hình từng ký tự, mỗi ký tự trên một dòng.
+# Đếm và thông báo ra màn hình:
+# Số lượng ký tự là chữ cái (không phân biệt hoa, thường).
+# Số lượng ký tự là chữ số.
+# Số lượng nguyên âm (a, e, i, o, u) có trong chuỗi.
 chuoi_van_ban = input("Nhập chuỗi văn bản bất kỳ: ")
 letter_count = 0
 digit_count = 0
